18445607276466d7f21a855055b210d52924e289
Sviatlana Vansovich <yan>
2024-06-03 21:24:11 +0300

    f.com

---
+++ index.js
@@ -0,0 +1,20 @@
+const startBtn = document.getElementById('start')
+const cards = document.querySelectorAll('.card')
+const timeController = document.querySelector('.time-list')
+const modal = document.getElementById('modal')
+const board = document.getElementById('board')
+const time = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+
+
+
+console.log(startBtn);
+console.log(cards);
+console.log(timeController);
+console.log(modal);
+console.log(board);
+console.log(time);
+console.log(scoreEl);
+
+
+

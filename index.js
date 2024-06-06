0fd23b0ba30d8809ab93a67a9e0219bdf094ddf7
Sviatlana Vansovich <yan>
2024-06-06 21:16:30 +0300

    s.com

--- index.js
+++ index.js
@@ -1,20 +1,61 @@
-const startBtn = document.getElementById('start')
-const cards = document.querySelectorAll('.card')
-const timeController = document.querySelector('.time-list')
-const modal = document.getElementById('modal')
-const board = document.getElementById('board')
-const time = document.getElementById('time')
+const startBtnEl = document.getElementById('start')
+const cardsEl = document.querySelectorAll('.card')
+const timeControllerEl = document.querySelector('.time-list')
+const modalEl = document.getElementById('modal')
+const boardEl = document.getElementById('board')
+const timeEl = document.getElementById('time')
 const scoreEl = document.getElementById('score')
 
+let selectedTime = null
+let time = null
+let idSetInterval = null
 
+function handlerStartBtn(e) {
+    e.preventDefault() 
+    cardsEl[0].classList.add('up')
+}
 
-console.log(startBtn);
-console.log(cards);
-console.log(timeController);
-console.log(modal);
-console.log(board);
-console.log(time);
-console.log(scoreEl);
+startBtnEl.addEventListener('click', handlerStartBtn)
 
+function handlerTimeController(e) {
+
+    if(e.target.classList.contains('time-list__item')){
+        cardsEl[1].classList.add('up')
+        
+        selectedTime = parseInt(e.target.dataset.time)
+        time = selectedTime
+        startGame()
+        console.log(time);
+    }
+}
+
+timeControllerEl.addEventListener('click', handlerTimeController)
+
+function startGame () {
+   setTime(time)
+   idSetInterval = setInterval(decTime, 1000)
+}
+
+function finishGame() {
+    console.log('Game over'); 
+    clearInterval(idSetInterval)
+}
+
+function decTime () {
+    if (time === 0) {
+        finishGame ()
+    } else {
+        let current = --time
+        // current = current < 10 ? "0" + current : current;
+        if(current < 10) {
+            current = '0' + current
+        }
+        setTime(current)
+    }
+}
+
+function setTime(timeGame) {
+    timeEl.innerHTML = `00:${timeGame}`
+}
 
 
